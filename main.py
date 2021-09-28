5c94bdcbc99a6992164def6612e30a4db1fe48b9
DrAlexSanz <52710912+DrAlexSanz@users.noreply.github.com>
2021-09-28 15:04:02 +0200

    Update main.py
    Typo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 import os
-os.chdir(r"C:\Users\asanz\Downloads\eMule\Incoming")
+os.chdir(r"C:\Users\MY_DIR")
 start_time = 2640 # Time in seconds
 end_time = 3240
-test_vid = ffmpeg_extract_subclip(r"Del44al54.avi", start_time, end_time, targetname="Anita Mendoza - Viaggio nel tempo.mp4")
+test_vid = ffmpeg_extract_subclip(r"Del44al54.avi", start_time, end_time, targetname=r"C:\Users\MY_FILE.mp4")
